cc5b4b3fec23d74a7ecbe3a9d289c0cb8b25b8f6
Shivansh jasathi <116382647+Shivxnshjasathi@users.noreply.github.com>
2024-08-26 02:25:53 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,89 +1,82 @@
-import { PEOPLE_URL } from "@/constants";
-import Image from "next/image";
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from '@/constants'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
 
-interface CampProps {
-  backgroundImage: string;
-  title: string;
-  subtitle: string;
-  peopleJoined: string;
+const Footer = () => {
+  return (
+    <footer className="flexCenter mb-24">
+      <div className="padding-container max-container flex w-full flex-col gap-14">
+        <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
+          <Link href="/" className="mb-10">
+            <Image src="hilink-logo.svg" alt="logo" width={74} height={29}/>
+          </Link>
+
+          <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
+            {FOOTER_LINKS.map((columns, index) => (
+              <FooterColumn title={columns.title} key={`footer-column-${index}`}>
+                <ul className="regular-14 flex flex-col gap-4 text-gray-30">
+                  {columns.links.map((link, linkIndex) => (
+                    <Link href="/" key={`footer-link-${linkIndex}`}>
+                      {link}
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColumn>
+            ))}
+
+            <div className="flex flex-col gap-5">
+              <FooterColumn title={FOOTER_CONTACT_INFO.title} key="footer-contact">
+                {FOOTER_CONTACT_INFO.links.map((link, linkIndex) => (
+                  <Link
+                    href="/"
+                    key={`contact-link-${linkIndex}`}
+                    className="flex gap-4 md:flex-col lg:flex-row"
+                  >
+                    <p className="whitespace-nowrap">
+                      {link.label}:
+                    </p>
+                    <p className="medium-14 whitespace-nowrap text-blue-70">
+                      {link.value}
+                    </p>
+                  </Link>
+                ))}
+              </FooterColumn>
+            </div>
+
+            <div className="flex flex-col gap-5">
+              <FooterColumn title={SOCIALS.title} key="footer-socials">
+                <ul className="regular-14 flex gap-4 text-gray-30">
+                  {SOCIALS.links.map((link, linkIndex) => (
+                    <Link href="/" key={`social-link-${linkIndex}`}>
+                      <Image src={link} alt="logo" width={24} height={24} />
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColumn>
+            </div>
+          </div>
+        </div>
+
+        <div className="border bg-gray-20" />
+        <p className="regular-14 w-full text-center text-gray-30">2023 Hilink | All rights reserved</p>
+      </div>
+    </footer>
+  )
 }
 
-const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps) => {
-  return (
-    <div className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
-     <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
-      <div className="flexCenter gap-4">
-        <div className="rounded-full bg-green-50 p-4">
-          <Image
-            src="/folded-map.svg"
-            alt="map"
-            width={28}
-            height={28}
-          />
-        </div>
-        <div className="flex flex-col gap-1">
-          <h4 className="bold-18 text-white">{title}</h4>
-          <p className="regular-14 text-white">{subtitle}</p>
-        </div>
-      </div>
+type FooterColumnProps = {
+  title: string;
+  children: React.ReactNode;
+}
 
-      <div className="flexCenter gap-6">
-        <span className="flex -space-x-4 overflow-hidden">
-          {PEOPLE_URL.map((url) => (
-            <Image 
-              className="inline-block h-10 w-10 rounded-full"
-              src={url}
-              key={url}
-              alt="person"
-              width={52}
-              height={52}
-            />
-          ))}
-        </span>
-        <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
-      </div>
-     </div>
+const FooterColumn = ({ title, children }: FooterColumnProps) => {
+  return (
+    <div className="flex flex-col gap-5">
+      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
+      {children}
     </div>
   )
 }
 
-const Camp = () => {
-  return (
-    <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
-      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
-        <CampSite 
-          backgroundImage="bg-bg-img-1"
-          title="Putuk Truno Camp"
-          subtitle="Prigen, Pasuruan"
-          peopleJoined="50+ Joined"
-        />
-        <CampSite 
-          backgroundImage="bg-bg-img-2"
-          title="Mountain View Camp"
-          subtitle="Somewhere in the Wilderness"
-          peopleJoined="50+ Joined"
-        />
-      </div>
-
-      <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
-        <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
-          <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white">
-            <strong>Feeling Lost</strong> And Not Knowing The Way&#39;?
-          </h2>
-          <p className="regular-14 xl:regular-16 mt-5 text-white">
-            Starting from the anxiety of the climbers when visiting a new climbing location, the possibility of getting lost is very large. That&apos;s why we are here for those of you who want to start an adventure
-          </p>
-          <Image 
-            src="/quote.svg"
-            alt="camp-2"
-            width={186}
-            height={219}
-            className="camp-quote"
-          />
-        </div>
-      </div>
-    </section>
-  )
-}
-
-export default Camp;
+export default Footer;
